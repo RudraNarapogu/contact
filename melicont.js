bca4bf219aef04491d717d2fd3e7b0acd08dbd92
Rudra Narapogu <narapogurudra@gmail.com>
2024-07-31 22:58:58 +0530

    Add files via upload

---
+++ melicont.js
@@ -0,0 +1,44 @@
+document.getElementById('call-us').addEventListener('click', function() {
+    alert('Calling Us...');
+    // Future functionality can be added here
+});
+
+document.getElementById('chat').addEventListener('click', function() {
+    alert('Starting Chat...');
+    // Future functionality can be added here
+});
+
+document.getElementById('whatsapp').addEventListener('click', function() {
+    alert('Opening WhatsApp...');
+    // Future functionality can be added here
+});
+
+document.getElementById('request-call').addEventListener('click', function() {
+    alert('Requesting a Call...');
+    // Future functionality can be added here
+});
+
+document.getElementById('facebook').addEventListener('click', function() {
+    alert('Connecting to Facebook...');
+    // Future functionality can be added here
+});
+
+document.getElementById('twitter').addEventListener('click', function() {
+    alert('Connecting to Twitter...');
+    // Future functionality can be added here
+});
+
+document.getElementById('instagram').addEventListener('click', function() {
+    alert('Connecting to Instagram...');
+    // Future functionality can be added here
+});
+
+document.getElementById('email').addEventListener('click', function() {
+    alert('Connecting to Email...');
+    // Future functionality can be added here
+});
+
+document.getElementById('youtube').addEventListener('click', function() {
+    alert('Connecting to YouTube...');
+    // Future functionality can be added here
+});
